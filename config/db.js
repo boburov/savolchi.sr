bc814075e866f30b4ab15282b4ff6d02013bef58
Boburov Shukurullo <boburovdev@gmail.com>
2025-11-04 14:27:02 +0500

    schema created

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function connectDB() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully.");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+connectDB();
 
 const port = 3000;
 
