13d0c5720aa7325f1030b8fd39c92bca34088170
Boburov Shukurullo <boburovdev@gmail.com>
2025-11-29 23:17:53 +0500

    cors added

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://admin.savolchi.uz/',
+      'https://www.savolchi.uz/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
   const port = process.env.PORT || 5000;
+
   await app.listen(port, () => {
     Logger.log(`Nest application is running on: http://localhost:${port}`);
   });
