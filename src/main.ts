9d4a4ff3510e7d92b2ac25275ce96014b77da077
root <root@srv1156864.hstgr.cloud>
2025-12-02 19:23:50 +0000

    cors removed

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,6 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'https://admin.savolchi.uz/',
-      'https://www.savolchi.uz/',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-
   const port = process.env.PORT || 5000;
 
   await app.listen(port, () => {
